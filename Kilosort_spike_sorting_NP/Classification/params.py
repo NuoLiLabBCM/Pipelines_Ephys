88c278e47e320fa9f7f8c30ad2071e225d3818ff
Heet Kaku <35621575+heetkaku@users.noreply.github.com>
2024-01-23 10:47:39 -0600

    Update params.py
    Added thalamus classifier

--- Kilosort_spike_sorting_NP/Classification/params.py
+++ Kilosort_spike_sorting_NP/Classification/params.py
@@ -10,5 +10,13 @@ params = {'alm': {'model1_coef': np.expand_dims(np.array([0.0101489,3.72591,-0.2
                   'energy_threshold': 1e8,
                   'greater_equal': {'amplitude': 100, 'firing_rate': 0.2, 'presence_ratio': 0.95},
                   'less_equal': {'isi_viol': 0.1, 'amplitude_cutoff': 0.1, 'drift': 0.1}},
+          'thalamus':{
+              'model1_coef': np.expand_dims(np.array([0.0199491,2.12714,-0.890367,-8.15179,0.00236093,-0.113332,-0.084669,1.53893,-0.0153975,1.45377,0.0242426,-0.00188246,0.0692437,0.00760333,-0.228909]),0),
+              'model1_intercept': np.array([-1.79844293]), 
+              'model2_coef': np.expand_dims(np.array([0.0187705,4.18841,-1.16034,-2.73657,0.00703367,-0.126424,-0.102515,0.988925,0.178149,1.16077,0.00384977,-0.0064205,-0.109238,-0.000717697,-1.3162]),0),
+              'model2_intercept': np.array([-4.05561133]),             
+              'energy_threshold': 1e8,
+              'greater_equal': {'amplitude': 90, 'firing_rate': 0.1, 'presence_ratio': 0.90},
+              'less_equal': {'isi_viol': 0.05, 'amplitude_cutoff': 0.08, 'drift': 0.2}},
          'feat_cols': np.arange(1,16)}
           
